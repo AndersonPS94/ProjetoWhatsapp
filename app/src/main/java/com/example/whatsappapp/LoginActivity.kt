d4f24817cbc56c0a37325bb747c49cff243d1223
AndersonPS <dson_adn@hotmail.com>
2024-09-30 09:46:31 -0300

    aulas feitas -> 08/28

--- app/src/main/java/com/example/whatsappapp/LoginActivity.kt
+++ app/src/main/java/com/example/whatsappapp/LoginActivity.kt
@@ -7,17 +7,45 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.whatsappapp.databinding.ActivityLoginBinding
+import com.example.whatsappapp.utils.exibirMensagem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 
  class LoginActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
+
+     private lateinit var email: String
+     private lateinit var senha: String
+
+     private val firebaseAuth by lazy {
+         FirebaseAuth.getInstance()
+     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
         inicializarEventosClique()
+            firebaseAuth.signOut()
         }
+
+     override fun onStart() {
+         super.onStart()
+         verificarUsuarioLogado()
+     }
+
+     private fun verificarUsuarioLogado() {
+        val usuarioAtual = firebaseAuth.currentUser
+         if (usuarioAtual != null) {
+             startActivity(
+                 Intent(
+                     this,
+                     MainActivity::class.java)
+             )
+         }
+     }
 
      private fun inicializarEventosClique() {
          binding.textCadastro.setOnClickListener {
@@ -25,5 +53,48 @@ import com.example.whatsappapp.databinding.ActivityLoginBinding
                  CadastroActivity::class.java)
              )
          }
+           binding.btnLogar.setOnClickListener {
+               if (validarCampos()) {
+                   logarUsuario()
+           }
      }
  }
+
+     private fun logarUsuario() {
+         firebaseAuth.signInWithEmailAndPassword(
+             email, senha
+         ).addOnSuccessListener {
+             exibirMensagem("Logado com sucesso")
+             startActivity(Intent(this, MainActivity::class.java))
+         }.addOnFailureListener { erro ->
+             try {
+                 throw erro
+             } catch (erroAutenticacao: FirebaseAuthInvalidCredentialsException) {
+                 erroAutenticacao.printStackTrace()
+                 exibirMensagem("E-mail ou senha incorretos!")
+             } catch (erroUsuarioInvalido: FirebaseAuthInvalidUserException) {
+                 erroUsuarioInvalido.printStackTrace()
+                 exibirMensagem("Usuário inválido")
+             }
+         }
+     }
+
+     private fun validarCampos(): Boolean {
+         email = binding.editTextLoginEmail.text.toString()
+         senha = binding.editTextLoginSenha.text.toString()
+            if (email.isNotEmpty()) {
+                binding.textInputLayoutLoginEmail.error = null
+                if (senha.isNotEmpty()){
+                    binding.textInputLayoutLoginSenha.error = null
+                    return true
+                }else {
+                    binding.textInputLayoutLoginSenha.error = "Digite sua senha"
+                    return false
+                }
+            }else {
+                binding.textInputLayoutLoginEmail.error = "Digite seu e-mail"
+                    return false
+            }
+     }
+ }
+
